5b23fac3f24b64dd6bf97dc9bbceebd05686b7ec
meshreyas24 <42927270+meshreyas24@users.noreply.github.com>
2020-08-14 11:41:20 +0530

    Add files via upload
    Swift Playground Example:1

---
+++ SwiftPlayground1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+//declare variable str for string
+//== assignment operator
+//"" String To be displayed
+
+var str = "Hello, playground"
+
+//print to print string
+// \n New Line
+print(str)
+
+var a = 1
+var b = 1
+
+a = 2
+str = "hello"
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
